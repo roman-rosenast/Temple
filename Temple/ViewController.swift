4e55735986158d72f69c29842287a1481fd96e4a
Roman <rrosenast@cs.hmc.edu>
2019-05-30 13:47:14 -0700

    Initial Commit

---
+++ Temple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Temple
+//
+//  Created by Roman Rosenast on 5/30/19.
+//  Copyright © 2019 Roman Rosenast. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
